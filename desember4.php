5e2055d200a0951b8879a6ae00160bcad66dba91
Solveig Hansen <solveig@dx.no>
2016-12-07 09:23:05 +0100

    desember 4 done

--- desember4.php
+++ desember4.php
@@ -2,14 +2,15 @@
 
 $data = fopen('desember4_data.php', 'r') or die('Unable to open file...');
 
-$numRooms = 0;
 $sectorIDs = array();
+$alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+$alphabetLength = count($alphabet);
+$allRoomNames = array();
 
 while(!feof($data)) {
 	$line = fgets($data);
 
 	$letters = str_split($line);
-
 
 	if (count($letters) > 5) {
 		$obj = array();
@@ -30,25 +31,53 @@ while(!feof($data)) {
 		$sorted = array_keys($obj);
 
 		if ($checksum == $sorted[0] . $sorted[1] . $sorted[2] . $sorted[3] . $sorted[4]) {
-			$numRooms++;
-			$sectorIDs[] = preg_replace('/[^0-9]/', '', $line);
+
+			$sectorID = (int)preg_replace('/[^0-9]/', '', $line);
+			$sectorIDs[] = $sectorID; 
+
+			// Find how many times we need to shift each letter forward,
+			// as the length of the whole  alphabet will only get us back to our starting point
+			$rest = $sectorID % $alphabetLength;
+			
+			$str = substr($line, 0, $posChecksumStart - 3);
+			$strArr = str_split($str);
+
+			$decrypted = array();
+			foreach($strArr as $char) {
+				if ($char !== '-') {
+					$indexInAlphabet = array_search($char, $alphabet);
+					if ($indexInAlphabet) {
+						$decryptedIndex = $indexInAlphabet + $rest;
+						if ($decryptedIndex >= $alphabetLength) {
+							$decryptedIndex = $decryptedIndex - $alphabetLength;
+						}
+						$decrypted[] = $alphabet[$decryptedIndex];
+					}
+				} else {
+					$decrypted[] = ' ';
+				}
+			}
+			$allRoomNames[$sectorID] = implode('', $decrypted);
 		}
 	}
 
 }
 
-echo "Num rooms: " . $numRooms;
-echo "\n";
-echo "Sector ids: " .implode(' ', $sectorIDs);
+fclose($data);
+
+echo "Num rooms: " . count($sectorIDs) . "\n";
+echo "Sector ids: " .implode(' ', $sectorIDs) . "\n";
 
 $sumSectorIDs = 0;
 foreach($sectorIDs as $id) {
 	$sumSectorIDs += (int)$id;
 }
 
+echo "Sum sector ids: " . $sumSectorIDs . "\n";
+
+
+var_dump($allRoomNames);
 echo "\n";
-echo "Sum sector ids: " . $sumSectorIDs;
-
-fclose($data);
-
+// $matches = preg_grep('/stor/', $allRoomNames);
+// var_dump($matches);
 
